385b18eac6b24fc3a7b6e67818d989f5c20f1970
elassal33 <adham.elassal22@gmail.com>
2026-01-15 04:51:43 +0200

    done

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub_dashboard/core/helper_functions/on_generate_routes.dart';
+import 'package:fruit_hub_dashboard/core/services/get_it_service.dart';
+import 'package:fruit_hub_dashboard/core/services/supabase_stoarge.dart';
+import 'package:fruit_hub_dashboard/core/utils/keys.dart';
+import 'package:fruit_hub_dashboard/features/dashboard/views/dashboard_view.dart';
+import 'package:fruit_hub_dashboard/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/services/custom_bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SupabaseStoargeService.initSupabase();
+  await SupabaseStoargeService.createBuckets('fruits_images');
+  Bloc.observer = CustomBlocObserver();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  setupGetit();
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      initialRoute: DashboardView.routeName,
+      onGenerateRoute: onGenerateRoute,
+    );
+  }
+}
